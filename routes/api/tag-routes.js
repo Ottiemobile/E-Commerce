1e063f71b79f358dff590ca2018a8d04b04b3032
Ethan Torres <torresethan99@gmail.com>
2020-10-07 19:44:47 -0500

    product route needs fixing

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -20,7 +20,7 @@ router.get('/', (req, res) => {
 
 router.get('/:id', (req, res) => {
   // find a single tag by its `id`
-  Tag.findOne(req.body, {
+  Tag.findOne( {
     where: {
       id: req.params.id
     },
